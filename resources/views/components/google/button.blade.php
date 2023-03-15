db20fb7b8b5b561aecd70b2c5a48762e0d1dfddf
ItsClassified <karsmiesen@ziggo.nl>
2023-03-15 14:56:49 +0000

    Google button aangepast

--- resources/views/components/google/button.blade.php
+++ resources/views/components/google/button.blade.php
@@ -1,6 +1,6 @@
-<div id="g_id_onload" data-client_id="1091366472343-de1pvurdi6lipp6s2t65jcrcotr2vt57.apps.googleusercontent.com"  data-callback="GoogleSignInHandle" data-auto_prompt="true" wire:ignore>
+<div id="g_id_onload"  data-client_id="1091366472343-de1pvurdi6lipp6s2t65jcrcotr2vt57.apps.googleusercontent.com"  data-callback="GoogleSignInHandle" data-auto_prompt="true" wire:ignore>
 </div>
-<div wire:ignore class="g_id_signin pt-2 w-full" data-type="standard" data-size="large" data-theme="outline" data-text="sign_in_with" data-shape="rectangular" data-logo_alignment="left" data-width="325">
+<div wire:ignore class="g_id_signin w-full" data-type="standard" data-size="large" data-theme="outline" data-text="sign_in_with" data-shape="circular" data-logo_alignment="center" data-width="100%">
 </div>
 <script>
     function GoogleSignInHandle(response) {
